f72476ee0ea408545788f7f3e9bb965284d9e90b
Jamie Winsor <jamie@vialstudios.com>
2012-03-29 22:40:19 -0700

    add support for OSX to build-essential

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,10 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs C compiler / build tools"
 version           "1.0.0"
-recipe            "build-essential", "Installs C compiler and build tools on Linux"
+recipe            "build-essential", "Installs packages required for compiling C software from source."
 
-%w{ fedora redhat centos ubuntu debian amazon}.each do |os|
+%w{ fedora redhat centos ubuntu debian amazon }.each do |os|
   supports os
 end
+
+supports "mac_os_x", ">= 10.6.0"
