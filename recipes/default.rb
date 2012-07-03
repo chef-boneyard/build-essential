6377ae8f8e7c4204524e0060f34b34870df2ad22
Lamont Granquist <lamont@opscode.com>
2012-07-03 15:16:38 -0700

    add first-boot apt-get update when installing at compiletime

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,6 +24,19 @@ compiletime = node['build_essential']['compiletime']
 case node['os']
 when "linux"
 
+  # on apt-based platforms when first provisioning we need to force
+  # apt-get update at compiletime if we are going to try to install at compiletime
+  if node['platform_family'] == "debian"
+    execute "apt-get update" do
+      action :nothing
+      # tip: to suppress this running every time, just use the apt cookbook
+      not_if do
+        ::File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+        ::File.mtime('/var/lib/apt/periodic/update-success-stamp') > Time.now - 86400*2
+      end
+    end.run_action(:run) if compiletime
+  end
+
   packages = case node['platform_family']
     when "debian"
       %w{build-essential binutils-doc}
