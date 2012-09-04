d0e61ebed3608a8d1438789019fe9d37d8d3d54e
jtimberman <joshua@opscode.com>
2012-09-04 11:43:46 -0600

    [COOK-1620] - GCC installer for OS X 10.8

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,7 +22,8 @@ default['build_essential']['compiletime'] = false
 case platform
 when "mac_os_x"
   case
-  when Chef::VersionConstraint.new("~> 10.7.0").include?(platform_version)
+  when Chef::VersionConstraint.new("~> 10.7.0").include?(platform_version),
+       Chef::VersionConstraint.new("~> 10.8.0").include?(platform_version)
     default['build_essential']['osx']['gcc_installer_url'] = "https://github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg"
     default['build_essential']['osx']['gcc_installer_checksum'] = "df36aa87606feb99d0db9ac9a492819e"
   when Chef::VersionConstraint.new("~> 10.6.0").include?(platform_version)
