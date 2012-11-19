2bb0bfbf3d61a808d5860f8a92a0b7f99c19b3ec
jtimberman <joshua@opscode.com>
2012-11-19 16:24:36 -0700

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs C compiler / build tools"
-version           "1.2.0"
+version           "1.2.1"
 recipe            "build-essential", "Installs packages required for compiling C software from source."
 
 %w{ fedora redhat centos ubuntu debian amazon suse scientific}.each do |os|
