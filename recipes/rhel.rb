ce227db384d8f73bcda103627f689554d163f5a9
Seth Chisamore <schisamo@opscode.com>
2013-04-07 22:19:06 -0400

    [COOK-2505] ensure GCC 4 is available on older pre-6 EL

--- recipes/rhel.rb
+++ recipes/rhel.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-%w{
+pkgs = %w{
   autoconf
   bison
   flex
@@ -26,7 +26,14 @@
   kernel-devel
   make
   m4
-}.each do |pkg|
+}
+
+# ensure GCC 4 is available on older pre-6 EL
+if node['platform_version'].to_i < 6
+  pkgs.unshift %w{ gcc44 gcc44-c++ }
+end
+
+pkgs.flatten.each do |pkg|
 
   r = package pkg do
     action( node['build_essential']['compiletime'] ? :nothing : :install )
