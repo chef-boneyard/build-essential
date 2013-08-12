e15ce98237866f384ea71d704a379ffeff9c6afe
jtimberman <joshua@opscode.com>
2013-08-12 14:42:18 -0600

    [COOK-3318] - Use Chef::Mixin::ShellOut

--- recipes/mac_os_x.rb
+++ recipes/mac_os_x.rb
@@ -17,9 +17,10 @@
 # limitations under the License.
 #
 
-require 'chef/shell_out'
+require 'chef/mixin/shell_out'
+Chef::Recipe.send(:include, Chef::Mixin::ShellOut)
 
-result = Chef::ShellOut.new("pkgutil --pkgs").run_command
+result = shell_out("pkgutil --pkgs").run_command
 osx_gcc_installer_installed = result.stdout.split("\n").include?("com.apple.pkg.gcc4.2Leo")
 developer_tools_cli_installed = result.stdout.split("\n").include?("com.apple.pkg.DeveloperToolsCLI")
 pkg_filename = ::File.basename(node['build_essential']['osx']['gcc_installer_url'])
