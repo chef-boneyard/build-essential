28c61e2f97540fc82744d26f17b389d7ffb94fad
Seth Chisamore <schisamo@getchef.com>
2014-10-14 11:13:40 -0400

    Alphabetize `supports` by platform

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,17 @@ description       'Installs C compiler / build tools'
 version           '2.0.6'
 recipe            'build-essential', 'Installs packages required for compiling C software from source.'
 
-%w{ fedora redhat centos ubuntu debian amazon suse scientific oracle smartos}.each do |os|
-  supports os
-end
-
+supports 'amazon'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
 supports 'mac_os_x', '>= 10.6.0'
 supports 'mac_os_x_server', '>= 10.6.0'
+supports 'oracle'
+supports 'redhat'
+supports 'scientific'
+supports 'smartos'
+supports 'suse'
+supports 'ubuntu'
+
 suggests 'pkgutil' # Solaris 2
