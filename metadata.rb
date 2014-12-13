1d9cc85833257717eb3cb6a1e26e537bd6642f94
Chris Fordham <chris.fordham@industrieit.com>
2014-12-13 18:58:30 +1100

    Use ruby 1.9 syntax for compile_time attribute.

--- metadata.rb
+++ metadata.rb
@@ -23,7 +23,7 @@ supports 'ubuntu'
 suggests 'pkgutil' # Solaris 2
 
 attribute 'build-essential/compile_time',
-  :display_name => 'Build Essential Compile Time Execution',
-  :description => 'Execute resources at compile time.',
-  :default => false,
-  :recipes => ['build-essential::default']
+  display_name: 'Build Essential Compile Time Execution',
+  description: 'Execute resources at compile time.',
+  default: false,
+  recipes: ['build-essential::default']
