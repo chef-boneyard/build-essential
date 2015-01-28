cb87b65d9491477cd48b206c100fd04bc82b3916
keen99 <keen@icantclick.org>
2015-01-28 16:39:12 -0500

    be consistent, use single quotes.

--- metadata.rb
+++ metadata.rb
@@ -25,5 +25,5 @@ suggests 'pkgutil' # Solaris 2
 attribute 'build-essential/compile_time',
   display_name: 'Build Essential Compile Time Execution',
   description: 'Execute resources at compile time.',
-  default: "false",
+  default: 'false',
   recipes: ['build-essential::default']
