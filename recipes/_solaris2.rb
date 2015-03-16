a9ba525b166581011dd4a0c3b1fd2e59a2ca80c8
Scott Hain <shain@chef.io>
2015-03-16 13:01:36 -0700

    Solaris 10 build essential setup

--- recipes/_solaris2.rb
+++ recipes/_solaris2.rb
@@ -20,6 +20,46 @@
 case node['platform_version'].to_f
 when 5.10
   # install omnibus build essential package
+  potentially_at_compile_time do
+    # create a nocheck file for automated install
+    file '/var/sadm/install/admin/auto-install' do
+      content '
+mail=
+instance=overwrite
+partial=nocheck
+runlevel=nocheck
+idepend=nocheck
+space=ask
+setuid=nocheck
+conflict=nocheck
+action=nocheck
+basedir=default'
+      owner 'root'
+      group 'root'
+      mode '0444'
+    end
+
+    case node['kernel']['machine']
+    when 'i86pc'
+      package_url = 'https://chef-releng.s3.amazonaws.com/omnibus/build-essential/build-essential-0.0.4-1.i86pc.solaris'
+      package_checksum = 'bbee54df52109e1715f7acf4c3779c7a73765b8a8c74f7705f183556d57e6e3a'
+    when 'sun4v', 'sun4u'
+      package_url = 'https://chef-releng.s3.amazonaws.com/omnibus/build-essential/build-essential-0.0.4-1.sun4v.solaris'
+      package_checksum = 'c80a8365541cb713cedf1776ae731168a3f788be6f4ee1c85f7ff9cbfa8c530d'
+    end
+
+    package_name = File.basename(package_url)
+
+    remote_file "#{Chef::Config[:file_cache_path]}/#{package_name}" do
+      source package_url
+      checksum package_checksum
+    end
+
+    package 'build-essential' do
+      source "#{Chef::Config[:file_cache_path]}/#{package_name}"
+      options '-a auto-install'
+    end
+  end
 when 5.11
   potentially_at_compile_time do
     package 'autoconf'
