52dcfa118a7e9c950ab026a0d3b67cbe71e97297
Tim Smith <tsmith84@gmail.com>
2016-03-21 21:07:48 -0700

    Add specs for Solaris

--- spec/recipes/solaris2_spec.rb
+++ spec/recipes/solaris2_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe 'build-essential::_solaris2' do
-  pending 'fauxhai data for solaris2'
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new(platform: 'solaris2', version: '5.11')
+                          .converge(described_recipe)
+  end
+
+  it 'installs the correct packages' do
+    expect(chef_run).to install_package('autoconf')
+    expect(chef_run).to install_package('automake')
+    expect(chef_run).to install_package('bison')
+    expect(chef_run).to install_package('gnu-coreutils')
+    expect(chef_run).to install_package('flex')
+    expect(chef_run).to install_package('gcc')
+    expect(chef_run).to install_package('gcc-3')
+    expect(chef_run).to install_package('gnu-grep')
+    expect(chef_run).to install_package('gnu-make')
+    expect(chef_run).to install_package('gnu-patch')
+    expect(chef_run).to install_package('gnu-tar')
+    expect(chef_run).to install_package('pkg-config')
+    expect(chef_run).to install_package('ucb')
+  end
 end
