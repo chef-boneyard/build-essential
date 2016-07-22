3667ff1980bcd916d44e20aaa9ae617d57c824d0
Tim Smith <tsmith@chef.io>
2016-07-22 14:21:53 -0700

    Testing framework updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,7 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-ChefSpec::Coverage.start!
-
 RSpec.configure do |config|
-  config.order = 'random'
-  config.filter_run focus: true
-  config.run_all_when_everything_filtered = true
+  config.color = true
+  config.log_level = :error
 end
