0211c889dc8b63991d5f9b9ac03903688bdba1ab
Tim Smith <tsmith@chef.io>
2016-09-07 15:07:08 -0700

    Require the latest compat_resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -28,7 +28,7 @@ supports 'zlinux'
 
 depends 'seven_zip'
 depends 'mingw', '>= 1.1'
-depends 'compat_resource', '>= 12.10'
+depends 'compat_resource', '>= 12.14'
 
 source_url 'https://github.com/chef-cookbooks/build-essential'
 issues_url 'https://github.com/chef-cookbooks/build-essential/issues'
