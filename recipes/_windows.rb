1c413192ebf330f4d931e4d0828172aed9197ab6
Tim Smith <tsmith@chef.io>
2016-12-02 15:42:27 -0800

    Update copyright headers
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/_windows.rb
+++ recipes/_windows.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: build-essential
+# Cookbook:: build-essential
 # Recipe:: _windows
 #
-# Copyright 2016, Chef Software, Inc.
+# Copyright:: 2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- resources/xcode_command_line_tools.rb
+++ resources/xcode_command_line_tools.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: build-essential
+# Cookbook:: build-essential
 # Resource:: xcode_command_line_tools
 #
-# Copyright 2014-2016, Chef Software, Inc.
+# Copyright:: 2014-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
