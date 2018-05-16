3a507bddf525f3f2354a2aafa2c6826e73ed470c
Tim Smith <tsmith@chef.io>
2018-05-15 19:20:32 -0700

    Avoid ChefSpec deprecation warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/recipes/debian_spec.rb
+++ spec/recipes/debian_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe 'build-essential::default' do
-  let(:chef_run) do
-    ChefSpec::ServerRunner.new(platform: 'debian', version: '8.7',
-                               step_into: ['build_essential']).converge('build-essential::default')
-  end
+  context 'Debian' do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new(platform: 'debian', version: '8.9',
+                                 step_into: ['build_essential']).converge('build-essential::default')
+    end
 
-  it 'installs the correct packages' do
-    expect(chef_run).to install_package(['autoconf', 'binutils-doc', 'bison', 'build-essential', 'flex', 'gettext', 'ncurses-dev'])
+    it 'installs the correct packages' do
+      expect(chef_run).to install_package(['autoconf', 'binutils-doc', 'bison', 'build-essential', 'flex', 'gettext', 'ncurses-dev'])
+    end
   end
 end
--- spec/recipes/fedora_spec.rb
+++ spec/recipes/fedora_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe 'build-essential::default' do
-  let(:chef_run) do
-    ChefSpec::ServerRunner.new(platform: 'fedora', version: '25',
-                               step_into: ['build_essential']).converge('build-essential::default')
-  end
+  context 'Fedora' do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new(platform: 'fedora', version: '28',
+                                 step_into: ['build_essential']).converge('build-essential::default')
+    end
 
-  it 'installs the correct packages' do
-    expect(chef_run).to install_package(['autoconf', 'bison', 'flex', 'gcc', 'gcc-c++', 'gettext', 'kernel-devel', 'make', 'm4', 'ncurses-devel', 'patch'])
+    it 'installs the correct packages' do
+      expect(chef_run).to install_package(['autoconf', 'bison', 'flex', 'gcc', 'gcc-c++', 'gettext', 'kernel-devel', 'make', 'm4', 'ncurses-devel', 'patch'])
+    end
   end
 end
