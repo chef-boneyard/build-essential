d8d1d696a808ad2c1a60354cd3c0a6a421858b54
Tim Smith <tsmith@chef.io>
2018-05-15 19:20:10 -0700

    Remove support for FreeBSD < 10
    FreeBSD is EOL both as a FreeBSD release and as a supported release on Chef. The fauxhai data is gone and we can no longer test it.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/build_essential.rb
+++ resources/build_essential.rb
@@ -36,8 +36,6 @@ action :install do
     package 'devel/autoconf'
     package 'devel/m4'
     package 'devel/gettext'
-    # Only install gcc on freebsd 9.x - 10 uses clang
-    package 'lang/gcc49' if node['platform_version'].to_i <= 9
   when 'mac_os_x'
     xcode_command_line_tools 'install'
   when 'omnios'
--- spec/recipes/freebsd_spec.rb
+++ spec/recipes/freebsd_spec.rb
@@ -1,30 +1,17 @@
 require 'spec_helper'
 
 describe 'build-essential::default' do
-  let(:chef_run) do
-    ChefSpec::ServerRunner.new(platform: 'freebsd', version: '10.3',
-                               step_into: ['build_essential']).converge('build-essential::default')
-  end
+  context 'FreeBSD 10+' do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new(platform: 'freebsd', version: '10.3',
+                                 step_into: ['build_essential']).converge('build-essential::default')
+    end
 
-  it 'installs the correct packages' do
-    expect(chef_run).to install_package('devel/gmake')
-    expect(chef_run).to install_package('devel/autoconf')
-    expect(chef_run).to install_package('devel/m4')
-    expect(chef_run).to install_package('devel/gettext')
-  end
-end
-
-describe 'build-essential::default' do
-  let(:chef_run) do
-    ChefSpec::ServerRunner.new(platform: 'freebsd', version: '9.3',
-                               step_into: ['build_essential']).converge('build-essential::default')
-  end
-
-  it 'installs the correct packages' do
-    expect(chef_run).to install_package('devel/gmake')
-    expect(chef_run).to install_package('devel/autoconf')
-    expect(chef_run).to install_package('devel/m4')
-    expect(chef_run).to install_package('devel/gettext')
-    expect(chef_run).to install_package('lang/gcc49')
+    it 'installs the correct packages' do
+      expect(chef_run).to install_package('devel/gmake')
+      expect(chef_run).to install_package('devel/autoconf')
+      expect(chef_run).to install_package('devel/m4')
+      expect(chef_run).to install_package('devel/gettext')
+    end
   end
 end
